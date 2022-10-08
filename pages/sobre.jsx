079608195bc400eb8b3340eb9358334bb114469b
isaquedesousa2 <isaqueleal35@gmail.com>
2022-10-07 23:16:08 -0300

    removendo '

--- pages/sobre.jsx
+++ pages/sobre.jsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import Footer from "../components/footer";
 import Menu from "../components/menu";
 
@@ -11,7 +10,7 @@ const Sobre = () => {
           <h1 className="text-4xl font-bold">Sobre</h1>
           <p className="mt-8">
             Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
+            industry. Lorem Ipsum has been the industrys standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type
             and scrambled it to make a type specimen book. It has survived not
             only five centuries, but also the leap into electronic typesetting,
@@ -22,10 +21,10 @@ const Sobre = () => {
             a long established fact that a reader will be distracted by the
             readable content of a page when looking at its layout. The point of
             using Lorem Ipsum is that it has a more-or-less normal distribution
-            of letters, as opposed to using 'Content here, content here', making
+            of letters, as opposed to using Content here, content here, making
             it look like readable English. Many desktop publishing packages and
             web page editors now use Lorem Ipsum as their default model text,
-            and a search for 'lorem ipsum' will uncover many web sites still in
+            and a search for lorem ipsum will uncover many web sites still in
             their infancy. Various versions have evolved over the years,
             sometimes by accident, sometimes on purpose (injected humour and the
             like).
